ba1a5baafb78a437da274af5a330fbb04902edf1
LapizdaSilva <83371798+LapizdaSilva@users.noreply.github.com>
2025-04-17 08:44:53 -0300

    Add files via upload

--- ToDoList.py
+++ ToDoList.py
@@ -1,27 +1,70 @@
-from datetime import datetime
-import json, os
+from datetime import datetime, timedelta
+import pyautogui
+import json, os, tkinter as tk
 
-ARQUIVO = "tarefas.json"
+NOME_ARQUIVO = "tarefas_nogui.json"
+LOCAL_ARQUIVO = r"C:\Users\BushG\OneDrive\Área de Trabalho\Programas\python\Python\Pessoal\ListaTarefaJSON\Save"
+
+ARQUIVO = os.path.join(LOCAL_ARQUIVO, NOME_ARQUIVO)
 
 def mostrar_tarefa_formatada(i, tarefa):
-    print(f"{i}. [{tarefa['status']}] {tarefa['titulo']} (Criado em: {tarefa['criado_em']})")
+    prazo = tarefa.get("prazo_limite", "Sem prazo")
+    print(f"{i}. [{tarefa['status']}] {tarefa['titulo']} {tarefa['id']} | \n (Criado em: {tarefa['criado_em']} | prazo: {prazo})\n ")
 
 def ver_tarefas(tarefas):
-    if tarefas:
-        print("\nSuas tarefas:")
-        for i, tarefa in enumerate(tarefas, start=1):
+
+    tarefas_vencidas = []
+    tarefas_nvencidas = []    
+    
+    for tarefa in tarefas: 
+        prazo = tarefa.get("prazo_limite")
+        if prazo and prazo != "Sem prazo":
+            try:
+                prazo_dt = datetime.strptime(prazo, "%Y-%m-%d %H:%M:%S")
+                if prazo_dt < datetime.now() and tarefa["status"] != "✅":
+                    tarefas_vencidas.append(tarefa)
+                else:
+                    tarefas_nvencidas.append(tarefa)
+            except ValueError:
+                print(f"⚠️ Prazo inválido na tarefa '{tarefa['titulo']}', ignorando para ordenação.")
+                tarefas_nvencidas.append(tarefa)
+                continue
+        else:   
+            tarefas_nvencidas.append(tarefa)
+            
+    if tarefas_nvencidas:
+        for i, tarefa in enumerate(tarefas_nvencidas, start=1):
             mostrar_tarefa_formatada(i, tarefa)
     else:
-        print("Nenhuma tarefa cadastrada")
+        print("Nenhuma tarefa não vencida")
+        
+    if tarefas_vencidas:
+        mensagem_alerta = "Tarefas vencidas:\n\n"
+        for i, tarefa in enumerate(tarefas_vencidas, start=1):
+            mensagem_alerta += f"{i}. {tarefa['titulo']} (Venceu em: {tarefa['prazo_limite']})\n"
+        pyautogui.alert(text=mensagem_alerta, title="⚠️ Tarefas Vencidas", button="OK")
+    else:
+        print("Nenhuma tarefa vencida")
 
 def add_tarefas(tarefas):
     titulo = input("Digite uma nova tarefa: ")
+    prazo_input = input(("Digite o prazo da tarefa (formato: YYYY-MM-DD HH:MM) ou deixe em branco para sem prazo: "))
+    
+    prazo = "Sem prazo"
+    if prazo_input.strip():
+        try:
+            prazo_dt = datetime.strptime(prazo_input.strip(), "%Y-%m-%d %H:%M")
+            prazo = prazo_dt.strftime("%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            print("⚠️ Prazo inválido! Use o formato: YYYY-MM-DD HH:MM")
+    
     novo_id = tarefas[-1]["id"] + 1 if tarefas else 1
     nova_tarefa = {
         "id": novo_id,
         "titulo": titulo,
         "status": "❌",
-        "criado_em": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        "criado_em": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "prazo_limite": prazo
     }
     tarefas.append(nova_tarefa)
     print("Tarefa adicionada com o ID:", novo_id)
@@ -30,12 +73,13 @@ def remov_tarefa(tarefas):
     if tarefas:
         ver_tarefas(tarefas)
         try:
-            ind = int(input("Digite o número da tarefa a ser removida: "))
-            if 1 <= ind <= len(tarefas):
-                removida = tarefas.pop(ind - 1)
-                print(f"Tarefa '{removida['titulo']}' foi removida.")
+            ID_remov = int(input("Digite o ID da tarefa a ser removida: "))
+            tarefa = next((t for t in tarefas if t["id"] == ID_remov), None)
+            if tarefa:
+                tarefas.remove(tarefa)
+                print(f"Tarefa '{tarefa['titulo']}' foi removida.")
             else:
-                print("Número inválido")
+                print("ID não encontrado")
         except ValueError:
             print("Entrada inválida. Digite um número.")
     else:
@@ -45,9 +89,9 @@ def marcar_prnt(tarefas):
     if tarefas:
         ver_tarefas(tarefas)
         try:
-            ind = int(input("Digite o número da tarefa a ser marcada como concluída: "))
-            if 1 <= ind <= len(tarefas):
-                tarefa = tarefas[ind - 1]
+            id_conclui = int(input("Digite o ID da tarefa a ser marcada como concluída: "))
+            tarefa = next((t for t in tarefas if t["id"] == id_conclui), None)
+            if tarefa:
                 tarefa["status"] = "✅"
                 print(f"Tarefa '{tarefa['titulo']}' marcada como concluída.")
             else:
@@ -61,9 +105,9 @@ def marcar_prog(tarefas):
     if tarefas:
         ver_tarefas(tarefas)
         try:
-            ind = int(input("Digite o número da tarefa a ser marcada como em progresso: "))
-            if 1 <= ind <= len(tarefas):
-                tarefa = tarefas[ind - 1]
+            id_prog = int(input("Digite o ID da tarefa a ser marcada como em progresso: "))
+            tarefa = next((t for t in tarefas if t["id"] == id_prog), None)
+            if tarefa:
                 tarefa["status"] = "⏳"
                 print(f"Tarefa '{tarefa['titulo']}' marcada como em progresso.")
             else:
@@ -77,11 +121,11 @@ def desmarcar(tarefas):
     if tarefas:
         ver_tarefas(tarefas)
         try:
-            ind = int(input("Digite o número da tarefa a ser desmarcada: "))
-            if 1 <= ind <= len(tarefas):
-                tarefa = tarefas[ind - 1]
+            id_desmarca = int(input("Digite o ID da tarefa a ser desmarcada: "))
+            tarefa = next((t for t in tarefas if t["id"] == id_desmarca), None)
+            if tarefa:
                 tarefa["status"] = "❌"
-                print(f"Tarefa '{tarefa['titulo']}' desmarcada como concluída.")
+                print(f"Tarefa '{tarefa['titulo']}' marcada como não concluída.")
             else:
                 print("Número inválido")
         except ValueError:
@@ -98,6 +142,24 @@ def carregar_tarefas():
 def salvar_tarefas(tarefas):
     with open(ARQUIVO, "w", encoding="utf-8") as f:
         json.dump(tarefas, f, ensure_ascii=False, indent=4)
+        
+def tempo_limite(tarefas):
+    print("\nTarefas com prazo vencido:")
+    agora = datetime.now()
+    vencidas = []
+
+    for tarefa in tarefas:
+        prazo = tarefa.get("prazo_limite")
+        if prazo:
+            prazo_dt = datetime.strptime(prazo, "%Y-%m-%d %H:%M:%S")
+            if prazo_dt < agora and tarefa["status"] != "✅":
+                vencidas.append(tarefa)
+    if vencidas:
+        for i, tarefa in enumerate(vencidas, start=1):
+            mostrar_tarefa_formatada(i, tarefa)
+    else:
+        print("Nenhuma tarefa com prazo vencido.")
+
 
 def menu():
     tarefas = carregar_tarefas()
@@ -109,16 +171,17 @@ def menu():
         4: ("Marcar Tarefa como Concluída", lambda: marcar_prnt(tarefas)),
         5: ("Marcar Tarefa em Progresso", lambda: marcar_prog(tarefas)),
         6: ("Desmarcar Tarefa", lambda: desmarcar(tarefas)),
-        7: ("Sair", None),
+        7: ("Ver Tarefas Com Prazo Vencido", lambda: tempo_limite(tarefas)),
+        8: ("Sair", None)
     }
 
     while True:
         print("\nMenu:")
-        for i in range(1, 8):
+        for i in range(1, 9):
             print(f"{i} - {opcoes[i][0]}")
         try:
             op = int(input("Escolha uma opção: "))
-            if op == 7:
+            if op == 8:
                 print("Saindo...")
                 salvar_tarefas(tarefas)
                 break
@@ -132,3 +195,5 @@ def menu():
             print("Entrada inválida. Digite um número.")
 
 menu()
+
+
